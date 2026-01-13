d7d63366cab2cba24a1f1fb86007d0fa55e7a278
atharv17-ui <atharv17@vt.edu>
2026-01-13 19:03:13 +0530

    API calls working

--- src/ingestion/race_data.py
+++ src/ingestion/race_data.py
@@ -1,4 +1,46 @@
 import fastf1
 import pandas as pd
 
-print("Imports are working.")
+# Enabling caching for faster subsequent loads (important for speed and API stability)
+fastf1.Cache.enable_cache('data/fastf1_cache')
+
+def load_race_data():
+    """
+    This is just a test load to make sure API calls are working.
+    
+    Load Bahrain 2023 race data and extract:
+    - Lap times
+    - Tire compounds
+    - Pit stop laps
+    """
+    session = fastf1.get_session(2023, 'Bahrain', 'R')
+    session.load()
+    
+    laps = session.laps
+    
+    laps_df = laps[[
+        "Driver",
+        "LapNumber",
+        "LapTime",
+        "Compound",
+        "Stint",
+        "PitOutTime",
+        "PitInTime"
+    ]].copy()
+
+    laps_df["LapTimeSeconds"] = laps_df["LapTime"].dt.total_seconds()
+    
+    pit_laps = laps_df[
+        laps_df["PitInTime"].notna() | laps_df["PitOutTime"].notna()
+    ][["Driver", "LapNumber", "PitInTime", "PitOutTime"]]
+    
+    return laps_df, pit_laps
+
+if __name__ == "__main__":
+    laps_df, pit_laps = load_race_data()
+
+    print("\n=== LAP DATA (HEAD) ===")
+    print(laps_df.head())
+
+    print("\n=== PIT LAPS (HEAD) ===")
+    print(pit_laps.head())
