9ba538d3bc675a9405c4c5f62564431dfeadadbf
atharv17-ui <atharv17@vt.edu>
2026-01-21 14:24:11 -0500

    comments for clean_race_data

--- src/ingestion/clean_race_data.py
+++ src/ingestion/clean_race_data.py
@@ -1,5 +1,6 @@
 ## Clean the race data
 ## Account for extra lap times due to pit stops, virtual SC and SC.
+## If lap is x when car pits, it becomes x+1 when it comes out of the pit lane.
 
 import fastf1
 import pandas as pd
